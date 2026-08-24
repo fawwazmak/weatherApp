e860942f73847d7b9dde021a6a578834754f6d0f
fawwazmak <fawwazmakinde47@gmail.com>
2026-08-24 11:34:56 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import heroImg from './assets/hero.png'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='bg-black text-white'>Hello</p>
+    </>
+  )
+}
+
+export default App
